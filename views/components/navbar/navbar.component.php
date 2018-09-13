25501a0e5621537a7e7a879eb4e8ba8a1d4d0e2b
RemcoDewlde <dewilderemco13@gmail.com>
2018-09-13 17:26:26 +0200

    initial commit

---
+++ views/components/navbar/navbar.component.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="views/components/navbar/navbar.style.css">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <i class="fab fa-pied-piper-alt"></i>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
+    </div>
+</nav>

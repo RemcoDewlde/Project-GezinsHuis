128a4b29f419f7684f420336ef5f31795d431be0
Remco <dewilderemco13@gmail.com>
2018-09-26 22:10:58 +0200

    navbar iets aangepast

--- views/components/navbar/navbar.component.php
+++ views/components/navbar/navbar.component.php
@@ -10,10 +10,10 @@
                 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">About us</a>
+                <a class="nav-link" href="#">Contact</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
+                <a class="nav-link" href="#">About us</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">evenementen</a>

669d2f182239df2656348729f4665f57437cde25
remco <dewilderemco13@gmail.com>
2018-10-05 19:18:27 +0200

    code cleaning

--- views/dashboard/dashboardgebruikers.view.php
+++ views/dashboard/dashboardgebruikers.view.php
@@ -12,10 +12,10 @@
 
 </head>
 <body>
-<?php
-require 'views/components/dashboard/dashboardnavbar/dashboardnavbar.component.php';
-require 'views/components/dashboard/dashboardgebruikers/dashboardgebruikers.component.php';
-?>
+    <?php
+        require 'views/components/dashboard/dashboardnavbar/dashboardnavbar.component.php';
+        require 'views/components/dashboard/dashboardgebruikers/dashboardgebruikers.component.php';
+    ?>
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>

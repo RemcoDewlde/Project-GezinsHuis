2d3e0cd596d8449f704479e8f0b50fdb324caa53
remco <dewilderemco13@gmail.com>
2018-10-08 17:29:25 +0200

    een paar classes toegevoegd

---
+++ core/Comments.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: remco
+ * Date: 08/10/2018
+ * Time: 17:19
+ */
+
+class Comments
+{
+    public $idCommenton;
+    public $comment;
+    public $votes;
+
+}

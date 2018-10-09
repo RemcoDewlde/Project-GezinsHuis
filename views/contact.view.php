cbb13861fadb5de1454571a3981bade79e2ee8cb
LucHuis <luchuistra@gmail.com>
2018-10-09 14:16:14 +0200

    Contact en About Us paginas begin gemaakt, Luc en Jerome

--- views/contact.view.php
+++ views/contact.view.php
@@ -1,7 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Project1</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+</head>
+<body>
 <?php
 /**
  * Created by PhpStorm.
- * User: remco
- * Date: 07/10/2018
- * Time: 14:56
+ * User: luchu
+ * Date: 4-10-2018
+ * Time: 09:21
  */
+require 'components/navbar/navbar.component.php';
+?>
+<div class="container">
+
+<h1 class="display-4"> Contact Informatie</h1>
+<br> <br> <br>
+<p >Als u meer vragen heeft over het project, dan kan u met ons contact opnemen via telefonisch contact of e-mail.</p>
+<br>
+<p>Telefoonnummer: <a href="https://9gag.com/gag/a4QKnyZ"> 19975304 </a></p>
+<br>
+<p>Email Address: <a href="https://9gag.com/gag/aoNRqpA">windesheim@windesheim.windesheim.nl<a></p>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+</div>
+<?php
+require 'views/components/footer/footer.component.php';
+?>
+</body>
+</html>

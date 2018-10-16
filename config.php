d8cd866ecc854313ed36c1206c52dfa4d770f194
InsertnameEx3 <you@example.com>
2018-10-16 09:51:45 +0200

    Dashboard

--- config.php
+++ config.php
@@ -1,16 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: remco
- * Date: 05/10/2018
- * Time: 21:11
- */
+
 return [
     'database' => [
         'name' => 'gezinshuis',
         'username' => 'root',
-        'password' => 'toor',
-        'connection' => 'mysql:host=localhost:3306',
+        'password' => 'root',
+        'connection' => 'mysql:host=127.0.0.1',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
         ]
